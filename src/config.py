f57448c848cf6a3a30f1a4405f98bed2cc8a0978
Aakash Yadav <Aakashsyadav1999@gmail.com>
2025-05-20 22:04:14 +0530

    Database config

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Add database connection configuration
+DATABASE_CONFIG = {
+    "server": "192.168.1.4,2021",
+    "username": "ray",
+    "password": "advicr49",
+    "database": "music_prediction_db",
+    "table": "music_data",
+    "connection_string": "DRIVER={ODBC Driver 17 for SQL Server};SERVER={server};UID={username};PWD={password};DATABASE={database}"
+}
