369d17539d5fac40680bc59dd4ecc5ac8cf82c80
Aakash Yadav <aakashsyadav1999@gmail.com>
2025-06-05 17:38:57 +0000

    config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 # Add database connection configuration
 DATABASE_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
+    "host": os.getenv("DB_HOST", "mysql_service"),
     "port": int(os.getenv("DB_PORT", 3306)),
     "user": os.getenv("DB_USER", "ray"),
     "password": os.getenv("DB_PASSWORD", "advicr49"),
